e0d61200da2ccf8be137ff0f4433894aa296b3cc
Raul Pereira <raul.pereira@gmail.com>
2024-11-20 12:01:08 -0300

    create agent

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+import json
 import os
+import pandas as pd
 
 from dotenv import load_dotenv
 
+from langchain import hub
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import Field, BaseModel
 from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain.agents import Tool
 from langchain.prompts import PromptTemplate
 from langchain.tools import BaseTool
 
 load_dotenv()
+
+def find_student_data(student):
+  data = pd.read_csv("documentos/estudantes.csv")
+  student_data = data[data["USUARIO"] == student]
+
+  if student_data.empty:
+    return {}
+  
+  return student_data.iloc[:1].to_dict()
 
 class StudentExtractor(BaseModel):
   student:str = Field("Nome do estudante informado, sempre em letras minúsculas. Exemplo: joão, carlos, joana, carla.")
@@ -19,7 +33,7 @@ class StudentData(BaseTool):
 
   def _run(self, input: str) -> str:
     llm = ChatOpenAI(
-      model="gpt-4o",
+      model="gpt-3.5-turbo",
       api_key=os.getenv("OPENAI_API_KEY")
     )
 
@@ -32,13 +46,33 @@ class StudentData(BaseTool):
     )
 
     chain = template | llm | parser
-
     response = chain.invoke({"input": input})
-    print(response)
 
-    return response["student"]
+    student = response["student"]
+    student = student.lower()
+    student_data = find_student_data(student)
+
+    return json.dumps(student_data)
 
 question = "Quais os dados da Ana?"
 
-response = StudentData().run(question)
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_API_KEY"))
+
+student_data = StudentData()
+
+tools = [
+  Tool(
+    name = student_data.name,
+    func = student_data.run,
+    description = student_data.description
+  )
+]
+
+prompt = hub.pull("hwchase17/openai-functions-agent")
+print(prompt)
+
+agent = create_openai_tools_agent(llm, tools, prompt)
+executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+response = executor.invoke({"input": question})
 print(response)
